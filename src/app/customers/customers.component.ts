257232b6aad526aa1a83a9408f5d74e4186e8535
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-05-24 22:24:01 +0100

    Added search to html + code quality

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CustomerService} from "../services/customer.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {Customer} from "../model/customer.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customers',
@@ -8,18 +11,24 @@ import {CustomerService} from "../services/customer.service";
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-  customers: any;
+  customers! : Observable<Array<Customer>>;
   errorMessage! : string;
-  constructor(private customerService: CustomerService) { }
+  searchFormGroup : FormGroup | undefined;
+  constructor(private customerService: CustomerService, private fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this.customerService.getCustomers().subscribe({
-      next : (data)=>{
-        this.customers=data;
-      },
-      error:(err)=>{
+    this.searchFormGroup=this.fb.group({
+      keyword : this.fb.control("")
+    });
+    this.customers=this.customerService.getCustomers().pipe(
+      catchError(err=>{
         this.errorMessage=err.message;
-      }
-    })
+        return throwError(err);
+      })
+    );
+  }
+
+  handleSearchCustomers() {
+
   }
 }
