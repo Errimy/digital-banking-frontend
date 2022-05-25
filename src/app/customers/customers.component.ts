1940094ffeecb47f49968d3d37d2d6cfe6ebcc8c
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-05-25 11:09:08 +0100

    new customer redirect to /customers, added alert before delete

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -34,6 +34,8 @@ export class CustomersComponent implements OnInit {
   }
 
   handleDeleteCustomer(c: Customer) {
+    let conf= confirm("Are you sure?")
+    if(!conf) return;
     this.customerService.deleteCustomer(c.id).subscribe({
       next : (value)=> {
         this.customers=this.customers.pipe(
